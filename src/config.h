e8b0819cfba1bf04584585e1ee199bb2f8e718a1
Vectorized <webby1111@hotmail.com>
2025-01-26 03:50:13 +0800

    Optimize (#1)

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #ifndef VANITY_CONFIG
 #define VANITY_CONFIG
 
-static int const MAX_ITERATIONS = 100000;
+static int const MAX_ITERATIONS = 1000000000;
 static int const STOP_AFTER_KEYS_FOUND = 100;
 
 // how many times a gpu thread generates a public key in one go
@@ -10,11 +10,14 @@ __device__ const int ATTEMPTS_PER_EXECUTION = 100000;
 __device__ const int MAX_PATTERNS = 10;
 
 // exact matches at the beginning of the address, letter ? is wildcard
-
 __device__ static char const *prefixes[] = {
-	"AAAAA",
-	"BBBBB",
+	"De1eg",
+	"De1ega",
+	"De1egat",
+	"De1egate"
 };
 
-
+// "_" to denote exact case
+// "@" to denote case insensitive
+__device__ static char const *prefix_ignore_case_mask = "_____@@@";
 #endif
